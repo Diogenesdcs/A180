d8fdcddfd5040e534a467fd772ea8c8a1d0939bc
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2022-08-21 18:35:44 -0300

    terceira_aula1

--- code4testers/arrays.rb
+++ code4testers/arrays.rb
@@ -1,15 +1,15 @@
 
-bandas = ["NIRVANA","SILVERCHAR","RED HOT","SHAMAN","OS TRAVESSOS"]
+bandas = ["NIRVANA","SILVERCHAR","RED HOT","SHAMAN","OS TRAVESSOS"] #arrays
 
-# puts bandas[0]
+# puts bandas[0] 
 # puts bandas[1]
 # puts bandas[2]
 # puts bandas[3]
 
-bandas.push ("bon jovi") #push adiciona no array
+bandas.push ("bon jovi")  #push adiciona no array
 bandas.delete ("RED HOT") #delete de array
 
 #puts bandas
-pagode =  bandas.find { |metodo| metodo == 'OS TRAVESSOS' } #cria variavel pagode, faz uma busca no array bandas e retorna
+pagode =  bandas.find { |metodo| metodo == 'OS TRAVESSOS' } #cria variavel pagode, faz uma busca no array bandas e retorna a busca
 
 puts pagode #imprime variavel pagodes
--- code4testers/hashes.rb
+++ code4testers/hashes.rb
@@ -1,15 +1,15 @@
 fernando = {nome: "Fernando", email: "fernando@gmail.com", Legal: true} #hashes
 
-# puts fernando[:nome]
+# puts fernando[:nome]     #imprimi campos dentro da hashe
 # puts fernando[:email]
 # puts fernando[:Legal]
 
 joao = {nome: "João", email: "João@gmail.com", Legal: true}
 # puts joao[:nome]
 # puts joao[:email]
+# puts joao[:Legal]
 
-
-pessoas = [fernando,joao]     #pessoas recebe os arrays
+pessoas = [fernando,joao]     #cria variavel pessoas recebe os arrays feitos em cima
 
 puts pessoas[0][:nome]        #imprime vavriavel pessoas posição 0 do array campo "nome"
 puts pessoas[1][:email]
